9a903880cf7ece213f83c9858f693b708a9959f1
Joshua Evans <ueva97@gmail.com>
2019-07-25 01:54:08 +0100

    created setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f :
+    long_description = f.read()
+
+setuptools.setup(
+    name = "barl-simpleoptions",
+    version = "0.0.1",
+    author = "Joshua Evans",
+    author_email = "jbe25@bath.ac.uk",
+    description = "A package which provides a simple framework for working with Options in Reinforcement Learning.",
+    long_description = long_description,
+    url = "https://github.com/Ueva/BaRL-SimpleOptions",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Natural Language :: English"
+    ]
+)
