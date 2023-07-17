254ba77b1dd7e20d861176be42b5f0485a611c29
akshilpatel <akshilpatel11@gmail.com>
2023-07-17 15:46:01 +0100

    Finish Diffusion Options up to the point of subgoal extraction.

--- simpleoptions/implementations/diffusion_options.py
+++ simpleoptions/implementations/diffusion_options.py
@@ -1,5 +1,3 @@
-from utils import create_graphs
-import scipy.sparse
 import scipy as sp
 import sys
 import matplotlib.pyplot as plt
@@ -16,125 +14,6 @@ from simpleoptions.implementations import GenericOptionGenerator
 from typing import List
 from tqdm import tqdm
 
-THETA = 1e-8
-
-# DETERMINISTIC and directed version.#
-####
-
-# 1. Get the adjacency matrix M.
-# 2. Compute the Directed laplacian.
-# 3. Compute the right and left eigenvalues and eigenvectors of the laplacian.
-# 4. Take the real parts of the eigenvalues and eigenvectors.
-# 5. Compute Degree diagonal matrix from adjacency matrix.
-# 6. Set temperature.
-
-
-## Diffusion computation ##
-# For every node:
-# For every eigenvector:
-#####
-
-
-def train_option(self, env: BaseEnvironment, option: DiffusionOption):
-    """
-    Takes an Eigenoption and trains its internal policy using Value Iteration.
-
-    Args:
-        env (BaseEnvionrment): The environment in which to train the Eigenoption policies.
-        option (Eigenoption): The option whose internal policy to train.
-    """
-    def _get_available_primitives(state) -> List:
-        return env.get_available_actions(state)
-
-    def _intrinsic_reward(state, next_state):
-        reward = option.pvf[next_state] - option.pvf[state]
-
-        if math.isclose(reward, 0, abs_tol=1e-6):
-            return 0
-        else:
-            return reward
-
-    env.reset()
-
-    policy = {}
-    values = {}
-
-    # Initialise values and policy.
-    for state in option.pvf.keys():
-        values[state] = 0
-
-    while True:
-        # Policy Evaluation Step.
-        while True:
-            delta = 0
-            for state in option.pvf.keys():
-                if env.is_state_terminal(state):
-                    continue
-
-                v_old = values[state]
-
-                action = policy[state]
-
-                next_state = env.get_successors(state, [action])[0]
-                reward = _intrinsic_reward(state, next_state)
-
-                if env.is_state_terminal(next_state):
-                    v_next = 0
-                else:
-                    v_next = values[next_state]
-
-                values[state] = reward + self.gamma * v_next
-
-                delta = max(delta, abs(v_old - values[state]))
-            if delta < THETA:
-                break
-
-        # Policy Improvement Step.
-        policy_stable = True
-        for state in option.pvf.keys():
-            if env.is_state_terminal(state):
-                continue
-
-            a_old = policy[state]
-
-            best_action = None
-            best_value = -np.inf
-            for action in _get_available_primitives(state):
-
-                next_state = env.get_successors(state, [action])[0]
-                reward = _intrinsic_reward(state, next_state)
-
-                if env.is_state_terminal(next_state):
-                    continue
-                else:
-                    v_next = values[next_state]
-
-                value = reward + self.gamma * v_next
-
-                if value > best_value:
-                    best_action = action
-                    best_value = value
-
-            policy[state] = best_action
-
-            if a_old != policy[state]:
-                policy_stable = False
-
-        if policy_stable:
-            break
-
-    # Add policy to graph for inspection.
-    stg = env.generate_interaction_graph()
-    for state in stg.nodes:
-        if state in values:
-            stg.nodes[state][f"PVF {option.pvf_id} Values"] = values[state]
-        if state in policy:
-            stg.nodes[state][f"PVF {option.pvf_id} Policy"] = str(
-                policy[state])
-    nx.write_gexf(stg, "eigen_test.gexf", prettyprint=True)
-
-    option.primitive_policy = policy
-
 
 class DiffusionOption(BaseOption):
     def __init__(self, env: BaseEnvironment, id: int):
@@ -145,14 +24,13 @@ class DiffusionOption(BaseOption):
         for state in env.get_state_space():
             for action in env.get_available_actions(state):
                 if action not in self.primitive_actions.keys():
-                    self.primitive_actions[action] = PrimitiveOption(
-                        action, env)
+                    self.primitive_actions[action] = PrimitiveOption(action, env)
 
     def initiation(self, state):
         return not self.termination(state)
 
     def termination(self, state):
-        return float(self.env.is_state_terminal(state))
+        return self.env.is_state_terminal(state) or state == self.subgoal_state
 
     def policy(self, state, test=False):
         return self.primitive_actions[self.primitive_policy[state]]
@@ -179,91 +57,149 @@ class DiffusionOption(BaseOption):
         return not self == other
 
 
+class DiffusitionOptionGenerator(GenericOptionGenerator):
+    def __init__(self, num_options: int, time_scale: int):
+        self.num_options = num_options
+        self.time_scale = time_scale
+
+    def generate_options(self, env: BaseEnvironment) -> None:
+        """
+        Generates a set of Diffusion Options for the given environment.
+
+        Args:
+            env (BaseEnvironment): Environment used to generate Diffusion Options.
+
+        Returns:
+            List: A list of the generated Diffusion options.
+        """
+        env.reset()
+        diffusion_options = self._generate_options(env)
+        return diffusion_options
+    
+    def _generate_options(self, env: BaseEnvironment):
+        # Generate the environment's state-transition graph and ensure that it is undirected.
+        stg, node_list, adj_mat, deg_mat, empty_rc = self._extract_graph_matrices(env)
+        e_vals, left_e_vecs, right_e_vecs, D_root = self._compute_eigendecomp(deg_mat, adj_mat)
+        f_vec = self.compute_f(e_vals, left_e_vecs, right_e_vecs, D_root)
+
+        f_idx = 0
+        f_dict = {}
+        for i, node in enumerate(node_list):
+            if i in empty_rc:
+                f_dict[node] = 0
+            else:
+                f_dict[node] = f_vec[f_idx]
+                f_idx += 1
+
+        print(f_dict)
+        nx.set_node_attributes(stg, values=f_dict, name="diffusion_score")
+        subgoals = self._detect_subgoals_from_graph(stg)
+        options = [DiffusionOption(env, subgoal) for subgoal in subgoals]
+        return options
+
+    def _compute_eigendecomp(self, D, adj_mat):
+        D_diag = np.diag(D)
+        D_root = np.diag(np.sqrt(D_diag))
+        D_root_inv = np.diag(1 / np.sqrt(D_diag))
+
+        N = D_root_inv @ (D-adj_mat) @ D_root_inv
+
+        _, R = scipy.linalg.polar(N, side="left")
+        e_vals, left_e_vecs, right_e_vecs = scipy.linalg.eig(R, left=True, right=True)
+
+        e_order = np.argsort(e_vals)
+        e_vals = np.real(e_vals[e_order])
+        left_e_vecs = np.real(left_e_vecs[:, e_order]).T
+        right_e_vecs = np.real(right_e_vecs[:, e_order]).T
+
+        return e_vals, left_e_vecs, right_e_vecs, D_root
+
+    def _extract_graph_matrices(self, env: BaseEnvironment):
+        stg = env.generate_interaction_graph()
+        undirected_stg = stg.to_undirected()
+        node_list = list(undirected_stg.nodes)
+
+        adj_mat = nx.adjacency_matrix(stg, nodelist=node_list).todense()
+
+        # Remove unused nodes.
+        empty_r = np.flatnonzero(adj_mat.sum(1) == 0)
+        empty_c = np.flatnonzero(adj_mat.sum(1) == 0)
+        empty_rc = np.union1d(empty_r, empty_c)
+        adj_mat = np.delete(adj_mat, empty_rc, axis=0)
+        adj_mat = np.delete(adj_mat, empty_rc, axis=1)
+
+        # Degree matrix
+        deg_mat = np.diag(np.sum(adj_mat, axis=1))
+
+        return undirected_stg, node_list, adj_mat, deg_mat, empty_rc
+
+    def compute_f(self, e_vals, left_e_vecs, right_e_vecs, D_root):
+        e_vals = 1 - 0.5 * e_vals
+        e_val_pow = e_vals ** self.time_scale
+        d_root_vec = np.diag(D_root)
+        f = np.zeros(right_e_vecs.shape[0])
+
+        for s in range(f.shape[0]):       
+            f_vec = 0
+            for i in range(1, len(e_vals)):
+                f_vec += e_val_pow[i] * left_e_vecs[i, s] * right_e_vecs[i]
+            f_vec /= d_root_vec[s]
+            f_vec *= d_root_vec
+            f[s] = np.linalg.norm(f_vec, ord=2)**2
+        return f
+
+    def _add_f_score_to_graph(self, stg, f_dict):
+        for node in f_dict:
+            stg.nodes[node]["diffusion_score"] = f_dict[node]
+        return stg
+
+    def _detect_subgoals_from_graph(self, stg):
+        subgoals = {}
+
+        for node in list(stg.nodes()):
+            is_local_maxima = True
+            neighbours = list(stg.neighbors(node))
+            if len(neighbours) == 0:
+                continue
+
+            for neighbour in list(neighbours):  # N.B. Only considers out_edges if stg is directed
+                if stg.nodes[neighbour]["diffusion_score"] > stg.nodes[node]["diffusion_score"]:
+                    is_local_maxima = False
+                    break
+
+            if is_local_maxima:
+                subgoals[node] = stg.nodes[node]["diffusion_score"]
+
+                # Only take the top K subgoals.
+                sorted_subgoals = dict(sorted(subgoals.items(), key=lambda x: x[1], reverse=True)[:self.num_options])
+                return list(sorted_subgoals.keys())
+
+    def train_option(self, env: BaseEnvironment, option: DiffusionOption):
+        """
+        Takes an Eigenoption and trains its internal policy using Value Iteration.
+
+        Args:
+            env (BaseEnvionrment): The environment in which to train the Eigenoption policies.
+            option (Eigenoption): The option whose internal policy to train.
+        """
+        def _get_available_primitives(state) -> List:
+            return env.get_available_actions(state)
+
+        def _pseudo_reward(next_state, option):
+            if next_state == option.subgoal:
+                return 1
+            else:
+                return -0.01
+
+        pass
+
 if __name__ == "__main__":
-    import numpy as np
+    from simpleenvs.envs.discrete_rooms import DiscreteXuFourRooms
 
-    x = np.array([[
-        2, -1, 0,
-        -1, 2, -1,
-        0, -1, 2
-    ]]).reshape(3, 3)
+    env = DiscreteXuFourRooms()
 
-    eigenvalues, eigenvectors = scipy.linalg.eigh(x)
+    option_generator = DiffusitionOptionGenerator(10, 8)
+    print(len(env.get_state_space()))
+    options = option_generator.generate_options(env)
 
-    print(eigenvalues)
-
-
-# env_name = 'maze_16_16.data'
-env_name = 'four_rooms_11_11.data'
-G, G2 = create_graphs(env_name)
-G = G.to_undirected()
-M = np.array(nx.adjacency_matrix(G).todense()).astype(
-    np.double)  # adjacency_matrix
-L = nx.directed_laplacian_matrix(nx.DiGraph(G))  # Laplacian
-e, eigv_l, eigv_r = scipy.linalg.eig(
-    L, left=True, right=True)  # Get eigenvectors of laplacian
-o = e.argsort()  # Sort eigenvalues
-e = e[o].real  # Make them real.
-eigv_l = eigv_l[:, o].real.T  # eigenvectors left
-eigv_r = eigv_r[:, o].real.T  # eigenvectors right
-diags = M.sum(axis=1)  # Get the degree of each node
-temperature = 8
-
-
-def compute_diffusion(temp, e, eigv_l, eigv_r, diags):
-    vals = []
-    # For every node, for every
-    for i in range(len(G.nodes)):
-        val = 0
-        for k in range(1, len(eigv_l)):
-            val += np.power(1-0.5*e[k], temp) * np.power(diags[i], -0.5) * \
-                eigv_l[k][i] * np.power(diags, 0.5)*eigv_r[k]
-        val = np.power(np.linalg.norm(val), 2)
-        vals.append(val)
-    return vals
-
-
-vals = compute_diffusion(temperature, e, eigv_l, eigv_r, diags)
-
-
-pos = nx.get_node_attributes(G, 'pos')
-nx.draw_networkx(G, pos=pos, node_color=vals, node_size=200, with_labels=True)
-plt.show()
-
-
-# stocastic example: modified M with an increased probability of moving up in the gridworld graph
-G2 = G2.to_undirected()
-M = np.array(nx.adjacency_matrix(G2).todense()).astype(np.double)
-
-# Make the graph stocastic
-for i in range(len(M)):
-    for k in range(len(M)):
-        if M[i][k] != 0 and i == k-1:
-            M[i][k] += 4
-
-
-rows_cols_to_delete = []
-for i in G2.nodes:
-    if i not in G.nodes:
-        rows_cols_to_delete.append(i)
-
-M = np.delete(M, rows_cols_to_delete, 0)
-M = np.delete(M, rows_cols_to_delete, 1)
-
-# Get the left and right eigenvectors of the laplacian
-e, eigv_l, eigv_r = scipy.linalg.eig(L, left=True, right=True)
-
-o = e.argsort()
-e = e[o].real  # eigenvalues
-
-eigv_l = eigv_l[:, o].real.T  # eigenvectors left
-eigv_r = eigv_r[:, o].real.T  # eigenvectors right
-
-
-diags = M.sum(axis=1)
-temperature = 8
-
-vals = compute_diffusion(temperature, e, eigv_l, eigv_r, diags)
-
-nx.draw_networkx(G, pos=pos, node_color=vals, node_size=200, with_labels=True)
-plt.show()
+    print(options)
