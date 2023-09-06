a78dda4c5c3ebda2b229535612f640d0cb13b10b
Tom <tmssmith@gmail.com>
2023-09-06 11:23:11 +0100

    Add subpackages to setup.py

--- simpleoptions/__init__.py
+++ simpleoptions/__init__.py
@@ -1,6 +1,4 @@
 from simpleoptions.option import BaseOption
 from simpleoptions.primitive_option import PrimitiveOption
-
 from simpleoptions.options_agent import OptionAgent
-
 from simpleoptions.environment import BaseEnvironment
