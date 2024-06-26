c7d863444080ac42a22a8be87dd11595df6bfbae
Joshua Evans <jbe25@bath.ac.uk>
2024-06-26 16:40:09 +0100

    Added TransitionMatrixBaseEnvironment

--- simpleoptions/environment.py
+++ simpleoptions/environment.py
@@ -1,4 +1,6 @@
 import copy
+import random
+
 import networkx as nx
 
 from typing import List, Set
@@ -25,13 +27,28 @@ class BaseEnvironment(ABC):
         self.current_state = None
 
     @abstractmethod
-    def step(self, action: Hashable) -> Tuple[Hashable, float, bool, dict]:
+    def reset(self, state: Hashable = None) -> Hashable:
+        """
+        This method initialises, or reinitialises, the environment prior to starting a new episode.
+        It returns an initial state.
+
+        Arguments:
+            state (Hashable, optional) -- The state to reset the environment to. Defaults to None, and resets the environment to a random initial state.
+
+        Returns:
+            Hashable -- An initial environmental state.
+        """
+        pass
+
+    @abstractmethod
+    def step(self, action: Hashable, state: Hashable = None) -> Tuple[Hashable, float, bool, dict]:
         """
         This method implements the one-step transition dynamics of the environment. Given an action,
         the environment transitions to some next state accordingly.
 
         Arguments:
             action (Hashable) -- The action for the agent to take in the current environmental state.
+            state (Hashable, optional) -- The state to take the given action in. Defaults to None, and uses the current environmental state.
 
         Returns:
             Hashable -- The next environmental state.
@@ -40,17 +57,6 @@ class BaseEnvironment(ABC):
             dict -- A dictionary containing information about the current episode.
 
         next_state, reward, terminal, info -- ENSURE ORDER IS CORRECT!!!
-        """
-        pass
-
-    @abstractmethod
-    def reset(self) -> Hashable:
-        """
-        This method initialises, or reinitialises, the environment prior to starting a new episode.
-        It returns an initial state.
-
-        Returns:
-            Hashable -- An initial environmental state.
         """
         pass
 
@@ -195,9 +201,12 @@ class BaseEnvironment(ABC):
         pass
 
     @abstractmethod
-    def get_successors(self, state: Hashable = None, actions: List[Hashable] = None) -> List[Hashable]:
+    def get_successors(
+        self, state: Hashable = None, actions: List[Hashable] = None
+    ) -> List[Tuple[Hashable, float, float]]:
         """
-        Returns a list of states which can be reached by taking an action in the given state.
+        Returns a list of next-states and rewards that can be reached by taking an action in the given state, and the probabilities
+        of transitioning to each of them.
         If no state is specified, a list of successor states for the current state will be returned.
 
         Args:
@@ -205,7 +214,7 @@ class BaseEnvironment(ABC):
             actions (List[Hashable], optional): The actions to test in the given state when searching for successors. Defaults to None (i.e. tests all available actions).
 
         Returns:
-            List[Hashable]: A list of states reachable by taking an action in the given state.
+            List[Tuple[Hashable, float, float]]: A list of next-states and rewards reachable by taking an action in the given state, and the probabilities of transitioning to them.
         """
         pass
 
@@ -214,7 +223,7 @@ class BaseEnvironment(ABC):
         Returns a NetworkX DiGraph representing the state-transition graph for this environment.
 
         Returns:
-            nx.DiGraph: A NetworkX DiGraph representing the state-transition graph for this environment. Nodes are states, edges are possible transitions, edges weights are one.
+            nx.DiGraph: A NetworkX DiGraph representing the state-transition graph for this environment. Nodes are states, edges are possible transitions, edge weights are one.
         """
 
         # Generates a list of all reachable states, starting the search from the environment's initial states.
@@ -231,7 +240,8 @@ class BaseEnvironment(ABC):
                     states.append(successor_state)
 
                     if not self.is_state_terminal(successor_state):
-                        for new_successor_state in self.get_successors(successor_state):
+                        new_successors = self.get_successors(successor_state)
+                        for (new_successor_state, _), _ in new_successors:
                             next_successor_states.append(new_successor_state)
 
             current_successor_states = copy.deepcopy(next_successor_states)
@@ -246,8 +256,44 @@ class BaseEnvironment(ABC):
             stg.add_node(state)
 
             # Add directed edge between node and its successors.
-            for successor_state in self.get_successors(state):
+            successors = self.get_successors(state)
+            for (successor_state, _), _ in successors:
                 stg.add_node(successor_state)
                 stg.add_edge(state, successor_state)
 
         return stg
+
+
+class TransititonMatrixBaseEnvironment(BaseEnvironment):
+    def __init__(self, deterministic: bool = True):
+        self.deterministic = deterministic
+        super().__init__()
+
+        self.transition_matrix = self._compute_transition_matrix()
+
+    def _compute_transition_matrix(self):
+        # We want to create a dictionary representing the transition matrix for this environment.
+        # The dictionary should be keyed by state-action pair, and contain a list of (next_state, probability) tuples.
+
+        transition_matrix = {}
+
+        # We can iterate through each state in the state space using the get_state_space() method.
+        for state in self.get_state_space():
+            for action in self.get_available_actions(state=state):
+                transition_matrix[(state, action)] = self.get_successors(state=state, actions=[action])
+
+        return transition_matrix
+
+    def step(self, action, state=None):
+        # If the environment is deterministic, we can simply return the next state and reward.
+        if self.deterministic:
+            (next_state, reward), _ = self.transition_matrix[(self.current_state, action)][0]
+        # Otherwise, we sample a next state and reward based on the transition matrix.
+        else:
+            outcomes, probabilities = zip(*self.transition_matrix[(self.current_state, action)])
+            (next_state, reward), _ = random.choices(outcomes, probabilities)
+
+        # Determine whether the next state is terminal.
+        terminal = self.is_state_terminal(next_state)
+
+        return next_state, reward, terminal, {}
